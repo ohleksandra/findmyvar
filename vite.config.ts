383bcafc218e609bd27ab10360ec99dd4edb7bd2
Oleksandra Huba <oleksandra.huba@gmail.com>
2025-12-05 21:06:28 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+/// <reference path="./src/vite-env.d.ts" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig(({ context }) => {
+	return {
+		plugins: context === 'ui' ? [react(), tailwindcss()] : [],
+	};
+});
