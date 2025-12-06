e1c8f1975d3f82fd3c82bda9f99049975b18514c
Denys Pupin <denyspupin.dev@gmail.com>
2025-12-06 22:49:28 +0100

    Message router is removed.

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,10 +1,6 @@
-import messageRouter from './messageRouter';
-
 export default function () {
 	const screenWidth = figma.viewport.bounds.width;
 	const pluginwidth = Math.max(900, Math.floor(screenWidth * 0.4));
 
 	figma.showUI(__html__, { width: pluginwidth, height: 600, themeColors: true });
-
-	figma.ui.onmessage = (msg) => messageRouter(msg);
 }
