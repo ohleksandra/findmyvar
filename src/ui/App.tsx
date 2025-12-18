411fa36c705eb2e54f6e9b00b0d90ab06af13d64
Denys Pupin <denyspupin.dev@gmail.com>
2025-12-18 02:36:06 +0100

    Add ProgressPane component and integrate progress tracking in App

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,21 +4,23 @@ import SearchResult from './components/search-result';
 import { useShallow } from 'zustand/react/shallow';
 import Header from './components/header';
 import Intro from './components/intro';
+import ProgressPane from './components/progress-pane';
+import { AnimatePresence } from 'motion/react';
 
 const App = () => {
-	const { fetchVariables, isSearching, searchResults, isSearchCompleted } = usePluginStore(
+	const { fetchVariables, isSearching, isSearchCompleted, progress } = usePluginStore(
 		useShallow((state) => ({
 			fetchVariables: state.fetchVariables,
 			isSearching: state.isSearching,
 			searchResults: state.searchResults,
 			isSearchCompleted: state.isSearchCompleted,
+			progress: state.progress,
 		})),
 	);
 
 	// Init
 	useEffect(() => {
 		fetchVariables();
-
 		const cleanup = initSearchListeners();
 		return cleanup;
 	}, [fetchVariables]);
@@ -26,6 +28,9 @@ const App = () => {
 	return (
 		<div className="flex flex-col w-full h-screen overflow-hidden">
 			<Header />
+			<AnimatePresence>
+				{progress && isSearching && <ProgressPane key="progress-pane" />}
+			</AnimatePresence>
 			{!isSearching && !isSearchCompleted && <Intro />}
 			{(isSearching || isSearchCompleted) && <SearchResult />}
 		</div>
---
+++ src/ui/components/progress-pane.tsx
@@ -0,0 +1,55 @@
+import { usePluginStore } from '@/store/plugin-store';
+import { useShallow } from 'zustand/react/shallow';
+import { Spinner } from './ui/spinner';
+import { Button } from './ui/button';
+import { Progress } from './ui/progress';
+import { useMemo } from 'react';
+
+const ProgressPane = () => {
+	const { searchQuery, progress, cancelSearch } = usePluginStore(
+		useShallow((state) => ({
+			searchQuery: state.searchQuery,
+			progress: state.progress,
+			cancelSearch: state.cancelSearch,
+		})),
+	);
+
+	const searchProgress = useMemo(
+		() => Math.floor(progress!.total > 0 ? (progress!.processed / progress!.total) * 100 : 0),
+		[progress],
+	);
+
+	return (
+		<div className="flex flex-col px-8 py-4 border-b border-b-[#E5E6E8] gap-y-4">
+			<div className="flex items-center">
+				<div className="bg-gray-50 border border-[#E5E6E8] rounded-lg p-1.5">
+					<Spinner className="size-6" />
+				</div>
+				<div className="flex flex-col pl-3">
+					<p className="font-sans text-sm font-medium">
+						Searching for{' '}
+						<span className="font-sans font-bold text-sm">'{searchQuery}'</span>
+					</p>
+					<p className="font-sans text-xs text-[#656B75]">
+						Analyzing your document structure and variable usage...
+					</p>
+				</div>
+				<div className="ml-auto">
+					<Button
+						variant="outline"
+						size="sm"
+						className="ml-auto"
+						onClick={async () => await cancelSearch()}
+					>
+						Cancel
+					</Button>
+				</div>
+			</div>
+			<div>
+				<Progress value={searchProgress} />
+			</div>
+		</div>
+	);
+};
+
+export default ProgressPane;
